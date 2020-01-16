392ef3ee1fd4dc3f03a7a82978025357ee4756bf
Duharter <duharter@ufl.edu>
2020-01-16 13:42:05 -0500

    Server connected

--- server/routes/ProductRoute.js
+++ server/routes/ProductRoute.js
@@ -1,8 +1,9 @@
-const examples = require('../controllers/examples.server.controller.js'),
+const product = require('../controllers/ProductController.js'),
     express = require('express'), 
     router = express.Router()
 
 router.route('/')
-  .get(examples.hello);
+  .get(product.list)
+  .post(product.create);
   
 module.exports = router;
