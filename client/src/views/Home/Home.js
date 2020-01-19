6f9ed301bfad28d205671ff48315a3a3139e11a6
Duharter <duharter@ufl.edu>
2020-01-19 14:11:59 -0500

    Removed extra stuff from home page and set up the navbar

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
 import './Home.css';
 
 function Home() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
+                {/* place logo here */}
                 <a
                     className="App-link"
                     href="https://reactjs.org"
