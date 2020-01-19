b8378618a0cdfe11e8a34e12810637f9a86a0920
Duharter <duharter@ufl.edu>
2020-01-19 14:26:53 -0500

    Products page draft

---
+++ client/src/views/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Products.css';
+
+const Products = () => {
+    return(
+        <div className="Products">
+            <header className="Products-header">
+                <a>Guac</a>
+            </header>
+        </div>
+    );
+}
+
+export default Products;
