f7c5b30bf9005804f6dadc9377551879eabb2e77
dbaltar27 <dbaltar27@ufl.edu>
2020-03-12 12:42:00 -0400

    cors changes

--- server/config/express.js
+++ server/config/express.js
@@ -24,12 +24,6 @@ module.exports.init = () => {
     // initialize app
     const app = express();
 
-    /*app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });*/
-
     var cors = require('cors');
 
     app.use(cors());
