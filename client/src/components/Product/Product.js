b513d63a73cd0094a6e8bb9f4e607990e73b7f64
AlexKatros <alexkatros@gmail.com>
2020-04-09 16:09:58 -0400

    functional description page

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -3,29 +3,61 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 //import Spinner from '../layout/Spinner';
 import { getProductByID } from '../../actions/product';
+import ProductCarousel from './ProductCarousel';
+import ProductDescription from './ProductDescription';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Product.css';
 
 const Product = ({
 	getProductByID,
 	product: { product, loading },
 	auth,
-	match
+	match,
 }) => {
 	useEffect(() => {
 		getProductByID(match.params.id);
 	}, [getProductByID]);
 
-	return <div>product</div>;
+	return (
+		<Fragment>
+			{product === null || loading ? (
+				<p>loading</p>
+			) : (
+				<Fragment>
+					<Link to='/products' class='btn btn-light'>
+						Back to Products
+					</Link>
+					<div class='container'>
+						<Container>
+							<Row>
+								<Col lg='7'>
+									<ProductCarousel product={product} />
+								</Col>
+								<Col lg='5'>
+									<ProductDescription product={product} />
+									<button type='submit' class='o-button-basket'>
+										<span class='c-product-add-to-cart__text'>Add to Cart</span>
+									</button>
+								</Col>
+							</Row>
+						</Container>
+					</div>
+				</Fragment>
+			)}
+		</Fragment>
+	); //TODO: If user = admin, add edit product button
 };
 
 Product.propTypes = {
 	getProductByID: PropTypes.func.isRequired,
 	product: PropTypes.object.isRequired,
-	auth: PropTypes.object.isRequired
+	auth: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
 	product: state.product,
-	auth: state.auth
+	auth: state.auth,
 });
 
 export default connect(mapStateToProps, { getProductByID })(Product);
